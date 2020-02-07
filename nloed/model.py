42bd94df18534e5735e09e64bdc4d8cd85cc23e4
NateBraniff <nbraniff@uwaterloo.ca>
2020-02-07 14:12:19 -0500

    implemented skeleton of model, model.design() and linear regression demo script linear.py

--- nloed/model.py
+++ nloed/model.py
@@ -1,33 +1,85 @@
-import casadi
+import casadi as cs
+#import math as mt
+import numpy as np
 
 class model:
 
-    def __init__(self, ylist, theta, alpha, beta, x):
+    def __init__(self, response,beta, x):
         
-        self.y=y
-        self.theta=theta
-        self.alpha=alpha
         self.beta=beta
         self.x=x
+        self.dist=[]
+        self.loglik=[]
+        self.fim=[]
 
-        for ytuple in self.ylist
+        #this is a counter to ensure different names for casadi symbols in each response
+        counter=0
 
-                 switch dist
-                     case 'Norm'
-                         
-                        #set logliklihood symbolics and function
-                        obj.pdf(i)= 1/sqrt(2*pi*obj.mu(2)) * exp(-(obj.y-obj.mu(1))^2./(2*obj.mu(2)));
-                        obj.pdf_func(i) = Function('pdf_func', {obj.y,obj.theta,obj.u}, {obj.pdf{i}});
-                        #set logliklihood symbolics and function
-                        obj.loglik(i) = log(obj.pdf);#%-0.5*log(2*pi*obj.mu(2)) - (obj.x-obj.mu(1))^2/(2*obj.mu(2));
-                        obj.loglik_func(i) = Function('loglik', {obj.y,obj.theta,obj.u}, {obj.loglik{i}});
-                        #set FIM symbolics and function
-                        mu_theta=jacobian(obj.mu,obj.theta);
-                        obj.fim(i)=(mu_theta(1,:)'*mu_theta(1,:))./obj.mu(2)+(mu_theta(2,:)'*mu_theta(2,:))./obj.mu(2)^2;
-                        obj.fim_func(i)=Function('fim', {obj.theta,obj.u}, {obj.fim{i}});
+        for r in response:
 
+            if r[0] =='normal':
 
+                #extract theta's from response list
+                mu=r[1][0]
+                sigma=r[1][1]
 
+                #create a response symbol
+                y=cs.SX.sym('y_'+str(counter),1)
+                #create loglikelihood symbolics and function 
+                logLikSymbol= -0.5*cs.log(2*cs.pi*sigma) - (y-mu)**2/(2*sigma)
+                self.loglik.append( cs.Function('ll_'+str(counter), [y,beta,x], [logLikSymbol]) )
+                #create FIM symbolics and function
+                dmu_dbeta=cs.jacobian(mu,beta)
+                dsigma_dbeta=cs.jacobian(sigma,beta)
+                fimSymbol=(dmu_dbeta.T @ dmu_dbeta)/sigma+(dsigma_dbeta.T @ dsigma_dbeta)/sigma**2
+                self.fim.append(cs.Function('fim_'+str(counter), [beta,x], [fimSymbol]) )
+            else:
 
+                print('error')
 
+        counter=counter+1
 
+    def design(self, beta0, xbounds):
+
+        nparameters=self.beta.size1()
+
+        xgrid=np.linspace(xbounds[0],xbounds[1],11)
+
+        xi0=np.ones(np.shape(xgrid))/xgrid.size
+        xi_list=[]
+        xi_sum=0
+
+        fim_sum=np.zeros((nparameters,nparameters) )
+
+        constr=[]
+        lowerboundxi = []
+        upperboundxi = []
+        lowerboundconstr = []
+        upperboundconstr = []
+
+        for k in range(xgrid.size):
+
+            xi_k=cs.SX.sym('xi_'+ str(k))
+            lowerboundxi += [0]
+            upperboundxi += [1]
+            xi_list += [xi_k]
+                
+            fim_sum= fim_sum + xi_k*self.fim[0](beta0,xgrid[k])
+            xi_sum=xi_sum+xi_k
+
+        constr+=[xi_sum-1]
+        lowerboundconstr += [0]
+        upperboundconstr += [0]
+        
+        #objective deff (include ds as well)
+        objective=-cs.log(cs.det(fim_sum))
+
+        # Create an NLP solver
+        prob = {'f': objective, 'x': cs.vertcat(*xi_list), 'g': cs.vertcat(*constr)}
+        solver = cs.nlpsol('solver', 'ipopt', prob)
+
+        # Solve the NLP
+        sol = solver(x0=xi0, lbx=lowerboundxi, ubx=upperboundxi, lbg=lowerboundconstr, ubg=upperboundconstr)
+        xi_opt = sol['x'].full().flatten()
+
+        return xi_opt
